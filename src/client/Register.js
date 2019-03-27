5566fd2213ea6e52da8e5f683936895cb8b89c3c
Simon Doyle <N00151932@iadt.ie>
2019-03-27 17:44:12 +0000

    first commit

---
+++ src/client/Register.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username : '',
+      password: '',
+      name: '',
+      age: ''
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    const { value, name } = event.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+    axios.post('/api/register', this.state)
+      .then(res => {
+        if (res.status === 200) {
+          this.props.history.push('/');
+        } else {
+          const error = new Error(res.error);
+          throw error;
+        }
+      })
+      .catch(err => {
+        console.error(err);
+        alert('Error registering in please try again');
+      });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmit}>
+        <h1>Register Below!</h1>
+        <input
+          type="text"
+          name="username"
+          placeholder="Enter username"
+          value={this.state.username}
+          onChange={this.handleInputChange}
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter password"
+          value={this.state.password}
+          onChange={this.handleInputChange}
+          required
+        />
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter name"
+          value={this.state.name}
+          onChange={this.handleInputChange}
+          required
+        />
+        <input
+          type="text"
+          name="age"
+          placeholder="Enter age"
+          value={this.state.age}
+          onChange={this.handleInputChange}
+          required
+        />
+        <input type="submit" value="Submit"/>
+      </form>
+    );
+  }
+}
